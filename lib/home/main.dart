b00eb0ff5d6e716c765ca72cdb091a37935fb5d8
Camilo Romero <cromero@yopapp.me>
2022-11-10 13:11:36 -0300

    added crash analytics

--- lib/home/main.dart
+++ lib/home/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
